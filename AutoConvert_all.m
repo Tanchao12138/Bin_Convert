304d64a83e6d6456fc4f5e1b7310270eed570fe6
Tanchao12138 <944298636@qq.com>
2025-05-28 15:42:46 +0800

    adapt to the current MPU6500_data_xxx.txt
    Signed-off-by: Tanchao12138 <944298636@qq.com>

--- AutoConvert_all.m
+++ AutoConvert_all.m
@@ -4,8 +4,7 @@ clc;                                % clear the command terminal
 
 % For data auto convert
 
-% d:\code_fan\matlab_\x-imu\open_source_debug\x-IMU-MATLAB-Library-9.1_debug\Testdatafile\
-fileFolder=fullfile('YISConvertDir');
+fileFolder=fullfile('IMUConvertDir');
 dirOutput=dir(fullfile(fileFolder,'*.TXT'));
 fileNames={dirOutput.name}';
 
@@ -14,73 +13,16 @@ formatOut = 'yymmdd';
 timestamp1 = datestr(now,formatOut);
 
 runcycle = size(fileNames,1);
-% fprintf(fileNames(1));
-% fan1 = char(fileNames(1));
-% fp=fopen(fan1,'rb');
 
 for i = 1:runcycle
     file = char(fileNames(i));
-    openfile = ['YISConvertDir\',file];
+    openfile = ['IMUConvertDir\',file];
 
-	sfile = strrep(file,'G00',['G',timestamp1,'_'])	;
-    savefile = ['YISConvertDir\YIS3_',sfile];
+	sfile = strrep(file,'data',[timestamp1,'_']);
+    savefile = ['IMUConvertDir\ZJUT_',sfile];
 	
-    run YISConvert;
-    fprintf([file,' converted\n']); 
-end
-
-fprintf('YIS file are converted\n');
-
-fileFolder=fullfile('MTiConvertDir');
-dirOutput=dir(fullfile(fileFolder,'*.TXT'));
-fileNames={dirOutput.name}';
-runcycle = size(fileNames,1);
-
-if 0  % 移花接木，还原成mtb文件，不好，不用了吧。
-
-% fprintf(fileNames(1));
-% fan1 = char(fileNames(1));
-% fp=fopen(fan1,'rb');
-
-% 移花接木，还原成mtb文件
-for i = 1:runcycle
-
-    file = char(fileNames(i));
-    openfile = ['MTiConvertDir\',file];
-    
-	sfile = strrep(file,'G00',['G',timestamp1,'_']);
-	sfile = strrep(sfile,'TXT','mtb');
-    savefile = ['MTiConvertDir\MTi3_',sfile];
-	
-	fid0=fopen(savefile,'w');
-	fid1=fopen('MT_2018-09-27_005_head.mtb','r');
-	fid2=fopen(openfile,'r');
-	head = fread(fid1);
-	data = fread(fid2);
-	fwrite(fid0,head);
-	fwrite(fid0,data);
-	fclose(fid0);
-	fclose(fid1);
-	fclose(fid2);
-	
-    fprintf([file,' resume MTB file OK \n']); 	
-end
-
-end
-
-% 这个还是不要了吧，没有姿态不行啊。
-for i = 1:runcycle
-    file = char(fileNames(i));
-    openfile = ['MTiConvertDir\',file];
-    
-	sfile = strrep(file,'G00',['G',timestamp1,'_']);
-	sfile = strrep(sfile,'TXT','txt');
-    savefile = ['MTiConvertDir\MTi3_',sfile];
-
     run MTiConvert;
     fprintf([file,' converted\n']); 
 end
-
-fprintf('MTi file are all resumed\n');
 
 fprintf('All file are converted\n');
--- MTiConvert.m
+++ MTiConvert.m
@@ -3,85 +3,28 @@
 fp=fopen(savefile,'w+');
 
 fprintf(fp,'// Start Time: Unknown\n');
-fprintf(fp,'// Update Rate: 100.0Hz\n');
-fprintf(fp,'// XSENS MTi-3 \n');
+fprintf(fp,'// Update Rate: 100.0Hz\200.0Hz\400.0Hz\n');
+fprintf(fp,'// ZJUT MPU6500 \n');
 fprintf(fp,'// Baudrate 115200 \n');
 fprintf(fp,'// Firmware Version: xxx\n');
-fprintf(fp,'PacketCounter	Acc_X	Acc_Y	Acc_Z	Gyr_X	Gyr_Y	Gyr_Z	Mag_X	Mag_Y	Mag_Z	Quat_q0	Quat_q1	Quat_q2	Quat_q3\n');
+fprintf(fp,'PacketCounter	Acc_X	Acc_Y	Acc_Z	Gyr_X	Gyr_Y	Gyr_Z\n');
     
 fid = fopen(openfile,'rb');
-% fid = fopen('ConvertDir\ELG00050_small.TXT','rb');
 while ~feof(fid)
     Header1 = fread(fid,1,'uint8');
     if Header1 == 250
         Header2 = fread(fid,1,'uint8');
-        Header3 = fread(fid,1,'uint8');
-        if ~isempty(Header3)  % ELG00079_org.TXT bug fix
-            if Header2 == 255 && Header3 == 54 % This is the start of a frame, data length 0x49
-                fseek(fid,4,'cof'); %skip two byte
-                pktID = fread(fid,1,'uint16',3,'b'); % skip three byte到四元数,默认为小端模式，这时要改成大端模式
-                quaternion = fread(fid,4,'single','b'); % read quaternion data
-                fseek(fid,3,'cof');
-                acc = fread(fid,3,'single','b');  % read accdata  
-                fseek(fid,3,'cof'); % skip 3 data, cof，表示当前位置
-                gyro = fread(fid,3,'single','b');% read gyro data
-                fseek(fid,3,'cof'); % 
-                mag = fread(fid,3,'single','b');% read mag data
-
-                if feof(fid) % isempty(mag(3)) % 改改 feof(fid) %若文件结束，则不打印这一帧数据了。
-                    break;
-                end
-
-                fprintf(fp,'%d\t%.6f\t%.6f\t%.6f\t%.6f\t%.6f\t%.6f\t%.6f\t%.6f\t%.6f\t%.6f\t%.6f\t%.6f\t%.6f\n',...
-                pktID,acc(1),acc(2),acc(3),gyro(1),gyro(2),gyro(3),mag(1),mag(2),mag(3),quaternion(1),quaternion(2),quaternion(3),quaternion(4));
-            else
-                fseek(fid,-2,'cof'); %还得退二格，不然会错过数据的,LOG00027.TXT bug fix
+            if Header2 == 255
+                pktID = fread(fid,1,'single'); % skip three byte到四元数,默认为小端模式，这时要改成大端模式
+                acc = fread(fid,3,'int16');  % read accdata  
+                gyro = fread(fid,3,'single');% read gyro data
+                fprintf(fp,'%.1f\t%.6f\t%.6f\t%.6f\t%.6f\t%.6f\t%.6f\n',...
+                pktID,acc(1),acc(2),acc(3),gyro(1),gyro(2),gyro(3));
             end
-        end
+     
     end   
 end
 fclose(fid);
 fclose(fp);
 
-% // Start Time: Unknown
-% // Update Rate: 100.0Hz
-% // Filter Profile: human (46.1)
-% // Option Flags: AHS Disabled ICC Disabled 
-% // Firmware Version: 4.3.1
-% PacketCounter	Acc_X	Acc_Y	Acc_Z	Gyr_X	Gyr_Y	Gyr_Z	Mag_X	Mag_Y	Mag_Z	Quat_q0	Quat_q1	Quat_q2	Quat_q3'
-
-% FA FF 
-% 36 
-% 49 总长度
-% 10 20 packet counter
-% 02 
-% 02 6F 
-% 20 10 四元数
-% 10 
-% 3A E7 D4 E7 q1
-% 3F 5D 46 FC q2
-% 3F 00 BB 7D q3
-% BA BC 96 49 q4
-% 40 20 加速度
-% 0C 
-% BC C4 A2 DB ax
-% 3D 35 B0 11 ay
-% C1 1C F4 6C az
-
-% 80 20 角速度
-% 0C 
-% 35 7A 00 02 gx
-% 38 7C 58 01 gy
-% BA AD BB 00 gz
-
-% C0 20 磁场强度
-% 0C 
-% 3F 16 B5 D4 magx
-% BE 9E 6B A0 magy
-% 3F 25 8B C8 magz
-
-% E0 10 状态byte
-% 01 
-% 03 状态byte
-% D8 校验和
 
