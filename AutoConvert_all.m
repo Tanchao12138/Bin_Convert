1825502ff2a608ea3893393105da4256921aebf7
Tanchao12138 <944298636@qq.com>
2025-06-02 19:14:56 +0800

    适配最新的嵌入式代码，添加了自动识别IMU种类以及采样率的信息
    Signed-off-by: Tanchao12138 <944298636@qq.com>

--- AutoConvert_all.m
+++ AutoConvert_all.m
@@ -15,7 +15,51 @@ timestamp1 = datestr(now,formatOut);
 runcycle = size(fileNames,1);
 
 for i = 1:runcycle
+    MPU6500 = 0;
+    ICM42688 = 0;
+    BMI088 = 0;    
+    SR_100 = 0;
+    SR_200 = 0;
+    SR_400 = 0;
+
     file = char(fileNames(i));
+
+    IMU_type = "MPU6500";
+    whether = any(contains(file, IMU_type));
+    if whether == 1
+        MPU6500 = 1;
+    end
+
+    IMU_type = "ICM42688";
+    whether = any(contains(file, IMU_type));
+    if whether == 1
+        ICM42688 = 1;
+    end
+
+    IMU_type = "BMI088";
+    whether = any(contains(file, IMU_type));
+    if whether == 1
+        BMI088 = 1;
+    end
+
+    SR = "100Hz";
+    whether = any(contains(file, SR));
+    if whether == 1
+        SR_100 = 1;
+    end
+
+    SR = "200Hz";
+    whether = any(contains(file, SR));
+    if whether == 1
+        SR_200 = 1;
+    end
+
+    SR = "400Hz";
+    whether = any(contains(file, SR));
+    if whether == 1
+        SR_400 = 1;
+    end
+
     openfile = ['IMUConvertDir\',file];
 
 	sfile = strrep(file,'data',[timestamp1,'_']);
--- Key_Convert.m
+++ Key_Convert.m
@@ -3,11 +3,24 @@
 fp=fopen(savefile,'w+');
 
 fprintf(fp,'// Start Time: Unknown\n');
-fprintf(fp,'// Update Rate: xxx\n');
-fprintf(fp,'// ZJUT MPU6500 \n');
-fprintf(fp,'// Baudrate 115200 \n');
-fprintf(fp,'// Firmware Version: xxx\n');
-fprintf(fp,'PacketCounter	Acc_X	Acc_Y	Acc_Z	Gyr_X	Gyr_Y	Gyr_Z\n');
+
+if SR_100 == 1
+    fprintf(fp,'// Sampling Rate: 100Hz\n');
+elseif SR_200 == 1
+    fprintf(fp,'// Sampling Rate: 200Hz\n');
+elseif SR_400 == 1
+    fprintf(fp,'// Sampling Rate: 400Hz\n');
+end
+
+if MPU6500 == 1
+    fprintf(fp,'// MPU6500 \n');
+elseif ICM42688 == 1
+    fprintf(fp,'// ICM42688 \n');
+elseif BMI088 == 1
+    fprintf(fp,'// BMI088 \n');
+end
+
+fprintf(fp,'Timestamp	Acc_X	Acc_Y	Acc_Z	Gyr_X	Gyr_Y	Gyr_Z\n');
     
 fid = fopen(openfile,'rb');
 while ~feof(fid)
@@ -15,8 +28,8 @@ while ~feof(fid)
     if Header1 == 250
         Header2 = fread(fid,1,'uint8');
             if Header2 == 255
-                pktID = fread(fid,1,'single'); % skip three byte到四元数,默认为小端模式，这时要改成大端模式
-                acc = fread(fid,3,'int16');  % read accdata  
+                pktID = fread(fid,1,'single'); 
+                acc = fread(fid,3,'int16');  % read acc data  
                 gyro = fread(fid,3,'single');% read gyro data
                 fprintf(fp,'%.1f\t%d\t%d\t%d\t%.6f\t%.6f\t%.6f\n',...
                 pktID,acc(1),acc(2),acc(3),gyro(1),gyro(2),gyro(3));
